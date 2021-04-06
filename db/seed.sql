a2192a15f8b06f82bafe648207b534413eed85a6
unknown <nukasine@gmail.com>
2021-04-06 15:54:33 -0600

    set up an sql connection and got env to function as it should.

--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,4 @@
 DROP DATABASE IF EXISTS manage_db;
 CREATE DATABASE manage_db;
+
+USE manage_db;
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const inquirer = require('inquirer');
-const db = require('./db')
-const mysql = require('mysql')
+// const db = require('./db')
+const mysql = require('mysql');
+require('dotenv').config()
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log(`We connected! Connected as thread id ${connection.thread}`)
+})
