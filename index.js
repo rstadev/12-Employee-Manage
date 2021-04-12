73593dcc1d550c4d78cfeaace413cd6e76eb34bc
unknown <nukasine@gmail.com>
2021-04-12 17:32:22 -0600

    should have updating file good to go. Creating an employee with no manager is an issue still, but will talk with tutor in regards to this.

--- index.js
+++ index.js
@@ -45,11 +45,13 @@ function database() {
           viewData(res.role);
           break;
         case 'Update employee roles':
-          if (res.role != 'Employee') {
+          if (res.role != 'Employees') {
             console.log('You picked something other than employee to update. Updating only workes with employee roles. Restarting program.')
             database();
           } else {
-            console.log(`You chose to update a ${res.role}`);
+            console.log(`You chose to update ${res.role}`);
+            updateData(res.role);
+            break;
           }
           break;
       };
@@ -62,18 +64,21 @@ function viewData(res) {
       connection.query('SELECT * FROM department', function (err, result) {
         if (err) throw err;
         console.table(result);
+        connectionEnd();
       });
       break;
     case 'Roles':
       connection.query('SELECT * FROM role', function (err, result) {
         if (err) throw err;
         console.table(result);
+        connectionEnd();
       });
       break;
     case 'Employees':
       connection.query('SELECT * FROM employee', function (err, result) {
         if (err) throw err;
         console.table(result);
+        connectionEnd();
       });
       break;
   }
@@ -118,6 +123,7 @@ function addData(res) {
             function (err, result) {
               if (err) throw err;
               console.log("Number of records inserted: " + result.affectedRows);
+              connectionEnd();
             }
           )
         });
@@ -132,7 +138,7 @@ function addData(res) {
         {
           name: 'salary',
           type: 'input',
-          message: 'What is the desired salary for this role? Respond in digits only.',
+          message: 'What is the desired salary for this role? Respond in numbers only.',
           // validate: async (input) => {
           //   if (input != isnum) {
           //     return 'Please select a number';
@@ -143,7 +149,7 @@ function addData(res) {
         {
           name: 'department_id',
           type: 'input',
-          message: 'What is the desired department_id related to this role? The associated department ID can be found by viewing your created departments. Digits only.'
+          message: 'What is the desired department_id related to this role? The associated department ID can be found by viewing your created departments. Integers only.'
         },
       ])
         .then(function (res) {
@@ -158,6 +164,7 @@ function addData(res) {
             function (err, result) {
               if (err) throw err;
               console.log("Number of records inserted: " + result.affectedRows);
+              connectionEnd();
             }
           )
         });
@@ -177,6 +184,7 @@ function addData(res) {
             function (err, result) {
               if (err) throw err;
               console.log("Number of records inserted: " + result.affectedRows);
+              connectionEnd();
             }
           )
         });
@@ -190,4 +198,78 @@ function addData(res) {
   //       console.table(result);
   //     })
   // }
-}
+};
+
+function updateData() {
+  connection.query('SELECT * FROM employee', function (err, res) {
+    const employeeList = res.map(obj => {
+      return {
+        name: `${obj.first_name} ${obj.last_name}`,
+        value: obj.id
+      }
+    });
+    connection.query('SELECT * FROM role', function (err, res) {
+      if (err) throw err;
+      const roleList = res.map(obj => {
+        return {
+          name: obj.title,
+          value: obj.id
+        }
+      });
+      inquirer.prompt([
+        {
+          name: 'employee_list',
+          type: 'list',
+          message: 'Which employee would you like to modify?',
+          choices: employeeList
+        },
+        {
+          name: 'role_list',
+          type: 'list',
+          message: 'What role would you like to assign to this employee?',
+          choices: roleList
+        }
+      ])
+        .then(function (res) {
+          connection.query(
+            'UPDATE employee SET ? WHERE ?',
+            [
+              {
+                role_id: res.role_list
+              },
+              {
+                id: res.employee_list
+              }
+            ],
+            function (err, result) {
+              if (err) throw err;
+              console.log("Number of records inserted: " + result.affectedRows);
+              connectionEnd();
+            }
+          )
+        })
+    });
+  });
+};
+
+function connectionEnd() {
+  inquirer.prompt({
+    name: 'continue',
+    type: 'list',
+    message: 'Would you like to exit now or return to the main menu?',
+    choices: [
+      'Return to Main Menu',
+      'Exit Program'
+    ]
+  })
+    .then(function (res) {
+      switch (res.continue) {
+        case 'Return to Main Menu':
+          database();
+          break;
+        case 'Exit Program':
+          connection.end();
+          break;
+      }
+    });
+};
